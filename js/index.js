4c9269f02eca42c918a1f48b8df3166c03fc76d6
sparshturkane <rajaturkane99@gmail.com>
2020-01-14 19:20:10 +0530

    changes by shreeya

--- js/index.js
+++ js/index.js
@@ -65,56 +65,56 @@ jQuery(document).ready(function($) {
     maxweight: "640",
     idvideo: "youtubeVideo"
   });
-  $("#enquiryFrom").validate({
-    rules: {
-      uname: "required",
-      companyname: "required",
-      serviceType: "required",
-      mobileNumeber: {
-        required: true,
-        number: true
-      },
-      email: {
-        required: true,
-        email: true
-      }
-    },
-    submitHandler: function(form, e) {
-      var first_name = $("#uname").val();
-      var companyname = $("#companyname").val();
-      var serviceType = $("#serviceType").val();
-      var mobileNumeber = $("#mobileNumeber").val();
-      var email = $("#email").val();
-      var pol = $("#pol").val();
-      var pod = $("#pod").val();
-      var dimensions = $("#dimensions").val();
-      var cweight = $("#cweight").val();
+  // $("#enquiryFrom").validate({
+  //   rules: {
+  //     uname: "required",
+  //     companyname: "required",
+  //     serviceType: "required",
+  //     mobileNumeber: {
+  //       required: true,
+  //       number: true
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     }
+  //   },
+  //   // submitHandler: function(form, e) {
+  //   //   var first_name = $("#uname").val();
+  //   //   var companyname = $("#companyname").val();
+  //   //   var serviceType = $("#serviceType").val();
+  //   //   var mobileNumeber = $("#mobileNumeber").val();
+  //   //   var email = $("#email").val();
+  //   //   var pol = $("#pol").val();
+  //   //   var pod = $("#pod").val();
+  //   //   var dimensions = $("#dimensions").val();
+  //   //   var cweight = $("#cweight").val();
 
-      var settings = {
-        async: true,
-        crossDomain: true,
-        url: "https://me.psychx86.com/allcargo/email-me.php",
-        method: "POST",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded"
-        },
-        data: {
-          first_name: first_name,
-          companyname: companyname,
-          serviceType: serviceType,
-          mobileNumeber: mobileNumeber,
-          pol: pol,
-          pod: pod,
-          dimensions: dimensions,
-          email: email,
-          cweight: cweight
-        }
-      };
-      $.ajax(settings).done(function(response) {
-        $("#successMsgModal").modal("show");
-        $("#enquiryFrom").trigger("reset");
-      });
-      return false;
-    }
-  });
+  //   //   var settings = {
+  //   //     async: true,
+  //   //     crossDomain: true,
+  //   //     url: "https://me.psychx86.com/allcargo/email-me.php",
+  //   //     method: "POST",
+  //   //     headers: {
+  //   //       "content-type": "application/x-www-form-urlencoded"
+  //   //     },
+  //   //     data: {
+  //   //       first_name: first_name,
+  //   //       companyname: companyname,
+  //   //       serviceType: serviceType,
+  //   //       mobileNumeber: mobileNumeber,
+  //   //       pol: pol,
+  //   //       pod: pod,
+  //   //       dimensions: dimensions,
+  //   //       email: email,
+  //   //       cweight: cweight
+  //   //     }
+  //   //   };
+  //   //   $.ajax(settings).done(function(response) {
+  //   //     $("#successMsgModal").modal("show");
+  //   //     $("#enquiryFrom").trigger("reset");
+  //   //   });
+  //   //   return false;
+  //   // }
+  // });
 });
